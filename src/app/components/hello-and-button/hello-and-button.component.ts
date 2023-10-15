93dd560ceeefe681169f19132be4d999f86c4ec1
Leonardo Tosoni <leodtosoni@gmail.com>
2023-10-15 03:48:21 -0300

    First trials of API Spotify - Get Adele's albums

--- src/app/components/hello-and-button/hello-and-button.component.ts
+++ src/app/components/hello-and-button/hello-and-button.component.ts
@@ -1,4 +1,27 @@
 import { Component } from '@angular/core';
+import { ServicioMusicaService } from 'src/app/services/servicio-musica.service';
+
+interface images {
+  height:string,
+  url:string,
+  width:string
+}
+
+interface albums {
+  type:string,
+  totalTracks:string,
+  availableMarkets:string,
+  externalsURL:string,
+  href:string,
+  id:string,
+  images:string[],
+  name:string,
+  releaseDate:string,
+  realeaseDatePrecision:string,
+  uri:string;
+  artists:string[],
+  albumGroup: string
+}
 
 @Component({
   selector: 'app-hello-and-button',
@@ -7,4 +30,31 @@ import { Component } from '@angular/core';
 })
 export class HelloAndButtonComponent {
 
+  imgAdele: string[] = []
+  albumsAdele:string[] = []
+
+  active:boolean = false
+
+  constructor(private api:ServicioMusicaService){
+
+  }
+
+  async obtenerAdeleImagesAndAlbums(){
+    this.active = true;
+    const artistID = await this.api.searchArtist()
+    const adeleImages = await this.api.getArtist(artistID)
+    adeleImages['images'].forEach((element:images) => {
+      this.imgAdele.push(element.url)    
+    });
+
+    const adeleAlbums = await this.api.getArtistAlbums(artistID)
+    adeleAlbums['items'].forEach((element:albums) => {
+      this.albumsAdele.push(element.name)
+      console.log(element)
+      
+    });
+
+/*     this.imgAdele.push(adele[images]) */
+    console.log("hello")
+  }
 }
--- src/app/services/servicio-musica.service.ts
+++ src/app/services/servicio-musica.service.ts
@@ -7,6 +7,78 @@ import { HttpClient } from '@angular/common/http'
 })
 export class ServicioMusicaService {
 
+  urlSpotifyToken:string = 'https://accounts.spotify.com/api/token'
+  clientID:string = 'd97b7a83e7a940638f380d1efcb63b5f'
+  clientSecret:string = 'fd634a32c1904ade834f5897cc52c6fd'
+  paramsToken:string = "?grant_type=client_credentials&client_id=" + this.clientID + "&client_secret=" + this.clientSecret
+  
+  header = { "Content-type" : 'application/x-www-form-urlencoded'} //No funciona todavia
+  urlSpotifyRequests: string = 'https://api.spotify.com/v1'
+
+  searchParams1:string = '?q=Adele&type=artist&market=US&limit=2' // Codigo recontra hardcodeado 
+  searchParams2:string = '/albums?include_groups=single&market=US&limit=10' // Codigo recontra hardcodeado
+  
+  async getTokenSpotify(){
+    try {
+       const request = await fetch(this.urlSpotifyToken + this.paramsToken, {
+        method: 'POST',
+        headers: {
+          "Content-type" : 'application/x-www-form-urlencoded'
+        }
+    });
+      const data = await request.json()
+      const token = await data['access_token']
+      return token;
+ 
+    } catch (error) {
+      console.log(error)      
+    }
+  } 
+  
+  
+  async searchArtist(){
+    try {
+      const token = await this.getTokenSpotify()
+      console.log(token)
+      const request = await fetch(this.urlSpotifyRequests + '/search/' + this.searchParams1,{
+        headers : {"Authorization" : "Bearer  " + token }
+      })
+      const data = await request.json()
+      const artistId = await data['artists']['items'][0]['id']
+      return artistId
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async getArtist(artistId:string){
+    try {
+      const token = await this.getTokenSpotify()
+      const request = await fetch(this.urlSpotifyRequests + '/artists/' + artistId ,{
+        headers : {"Authorization" : "Bearer  " + token }
+      })
+      const data = await request.json()
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  async getArtistAlbums(artistId:string){
+    try {
+      const token = await this.getTokenSpotify()
+      const request = await fetch(this.urlSpotifyRequests + '/artists/' + artistId +  this.searchParams2,{
+        headers : {"Authorization" : "Bearer  " + token }
+      })
+      const data = await request.json()
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  /* ESTO ES CODIGO DE LA API DE SHAZAM ANTES DE QUE FUNCIONARA LA API DE SPOTIFY */
   urlShazam: string = 'https://shazam.p.rapidapi.com/search?term=hello&locale=en-US&offset=0&limit=5';
 
   constructor(private http: HttpClient) { }
