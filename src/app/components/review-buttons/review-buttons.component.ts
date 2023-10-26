0b82809edabf17063715c936ea9d11a58d9bbb04
LucasGaunaDasmi <118076771+LucasGaunaDasmi@users.noreply.github.com>
2023-10-26 20:50:30 -0300

    Add files via upload

---
+++ src/app/components/review-buttons/review-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-buttons',
+  templateUrl: './review-buttons.component.html',
+  styleUrls: ['./review-buttons.component.css']
+})
+export class ReviewButtonsComponent {
+
+}
