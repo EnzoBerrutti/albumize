d79c9c654b290cba9906ec1664cf418bba8af5de
LucasGaunaDasmi <118076771+LucasGaunaDasmi@users.noreply.github.com>
2023-10-26 20:50:00 -0300

    Add files via upload

---
+++ src/app/components/album-info/album-info.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlbumInfoComponent } from './album-info.component';
+
+describe('AlbumInfoComponent', () => {
+  let component: AlbumInfoComponent;
+  let fixture: ComponentFixture<AlbumInfoComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AlbumInfoComponent]
+    });
+    fixture = TestBed.createComponent(AlbumInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
