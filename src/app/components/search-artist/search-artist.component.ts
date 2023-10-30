c4cc6f78c9c905c3ec8dcec96c3d852fdf452ecf
Leonardo Tosoni <leodtosoni@gmail.com>
2023-10-29 23:15:23 -0300

    Correccion de los estilos de los albumes

--- src/app/components/search-artist/search-artist.component.ts
+++ src/app/components/search-artist/search-artist.component.ts
@@ -8,35 +8,40 @@ import { ServicioMusicaService } from 'src/app/services/servicio-musica.service'
   templateUrl: './search-artist.component.html',
   styleUrls: ['./search-artist.component.css']
 })
-export class SearchArtistComponent implements OnInit{
+export class SearchArtistComponent implements OnInit {
   img: string[] = []
   albums: Album[] = []
   new_releases: Album[] = []
 
   idArtist = {} as number
+  busqueda = {} as string
 
   active: boolean = false
+
   formulario: FormGroup = this.fb.group({
     nombre: ''
   })
 
+
   constructor(private api: ServicioMusicaService,
     private fb: FormBuilder) { }
 
-    async ngOnInit(){
-      console.log("Hello")
-      const data = await this.api.getNewReleases()
-      this.new_releases = data['albums']['items']
-      this.getAlbumYear(this.new_releases)
-      this.getDottedName(this.new_releases)
-      console.log(this.new_releases)
-    }
+  async ngOnInit() {
+    console.log("Hello")
+    const data = await this.api.getNewReleases()
+    this.new_releases = data['albums']['items']
+    this.getAlbumYear(this.new_releases)
+    this.getDottedName(this.new_releases)
+    console.log(this.new_releases)
+  }
 
   async getAlbums() {
     this.albums = []
-    this.active = !this.active
-    const query = this.formulario.controls['nombre'].value
-    const data = await this.api.getAlbums(query)
+    if(this.active == false){
+      this.active = true
+    }
+    this.busqueda = this.formulario.controls['nombre'].value
+    const data = await this.api.getAlbums(this.busqueda)
     this.albums = data['albums']['items']
     this.getAlbumYear(this.albums)
     this.getDottedName(this.albums)
@@ -44,19 +49,23 @@ export class SearchArtistComponent implements OnInit{
 
   }
 
-getAlbumYear(array:Album[]){
-  array.forEach((item)=>{
-    item.release_date = item.release_date.split("-")[0]
-  })
-}
+  onKeyUpSearch(){
+    
+  }
 
-getDottedName(array:Album[]){
-  array.forEach((item:Album)=>{
-    if(item.name.length > 12){
-      item.name = item.name.slice(0,12) + '...'
-    }
-  })
-  
-}
+  getAlbumYear(array: Album[]) {
+    array.forEach((item) => {
+      item.release_date = item.release_date.split("-")[0]
+    })
+  }
+
+  getDottedName(array: Album[]) {
+    array.forEach((item: Album) => {
+      if (item.name.length > 12) {
+        item.name = item.name.slice(0, 12) + '...'
+      }
+    })
+
+  }
 
 }
