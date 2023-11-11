e0927eeb21617b5f75cc0231083397cbed492e1a
Leonardo Tosoni <leodtosoni@gmail.com>
2023-11-11 00:55:49 -0300

    Social media share buttons v2

--- src/app/components/share-buttons/share-buttons.component.ts
+++ src/app/components/share-buttons/share-buttons.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-share-buttons',
   templateUrl: './share-buttons.component.html',
@@ -9,43 +10,15 @@ export class ShareButtonsComponent {
   postUrl:String = encodeURI(window.location.href);
 
   url = 'www.youtube.com'
+  title = 'Albumize'
+  img = ''
+
   facebook = `https://www.facebook.com/sharer.php?u=${this.url}`
-
-  ngOnInit(){
-    /* console.log(this.postUrl) */
-    console.log(this.facebook)
-    
-  }
-  /* 
-  Social Share Links:
-  */
-    postFacebook(){
-      this.facebook = `https://www.facebook.com/sharer.php?u=${this.url}`
-    }
-
-    postInstagram(){
-
-    }
-    postWhatsapp(){
-
-/*       WhatsApp:
-      https://wa.me/?text=[post-title] [post-url] */
-    }
-    
-    postTwitter(){
-/*       Twitter:
-      https://twitter.com/share?url=[post-url]&text=[post-title] */
-    }
-    
-    postPinterest(){
-  /*     Pinterest:
-      https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is_video=[is_video]&description=[post-title] */
-    }
-    
-    postLinkedIn(){
- /*      LinkedIn:
-      https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title] */
-    }
+  whatsapp = `https://wa.me/?text=${this.title}&${this.url}`
+  twitter = `https://twitter.com/share?url=${this.url}&text=${this.title}`
+  pinterest = `https://pinterest.com/pin/create/bookmarklet/?media=${this.img}&url=${this.url}&is_video=${false}&description=${this.title}`
+  linkedin = `https://www.linkedin.com/shareArticle?url=${this.url}&title=${this.title}`
+  instagram = ``
     
           
 
