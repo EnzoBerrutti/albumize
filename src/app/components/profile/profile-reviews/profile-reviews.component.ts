7a2d19367c22065168dbea71f3a7172a6cccaf9e
Lucas Gauna Dasmi <lucasgauna1997@gmail.com>
2023-11-13 10:59:31 -0300

    sort en pagina del usuario

--- src/app/components/profile/profile-reviews/profile-reviews.component.ts
+++ src/app/components/profile/profile-reviews/profile-reviews.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Review, User } from 'src/app/interfaces/interfaces';
+import { CommonVariablesService } from 'src/app/services/common-variables.service';
 import { ReviewsService } from 'src/app/services/reviews.service';
 import { ServicioUsersService } from 'src/app/services/servicio-users.service';
 
@@ -16,11 +18,15 @@ export class ProfileReviewsComponent {
 
   reviewsArray:Review[] = []
 
+  formulario:FormGroup = this.fb.group({
+    selectedSort : 'opcion1'
+  })
+
   constructor(private activatedRoute:ActivatedRoute,
               private userAPI: ServicioUsersService,
-              private reviewsDB: ReviewsService){
+              private reviewsDB: ReviewsService,
+              private fb: FormBuilder){}
 
-  }
  async ngOnInit() {
     this.activatedRoute.params.subscribe((params:Params) =>{
       this.idUser = +params['idUser']})
@@ -31,8 +37,58 @@ export class ProfileReviewsComponent {
 
       this.reviewsArray = await allreviews.filter((r:Review) => r.reviewer === this.user.username)
 
-      this.reviewsArray.sort((b, a) => +new Date(a.date) - +new Date(b.date));
+      this.sortByMostRecent();
 
+  }
+
+  onSortBy() {
+    const selectedSort = this.formulario.controls['selectedSort'].value;
+  
+    switch (selectedSort) {
+      case 'opcion1':
+        this.sortByMostRecent();
+        break;
+      case 'opcion2':
+        this.sortByOldest();
+        break;
+      case 'opcion3':
+        this.sortFromHighToLow();
+        break;
+      case 'opcion4':
+        this.sortFromLowToHigh();
+        break;
+      default:
+        this.sortByMostRecent();
+        break;
+    }
+  }
+
+  sortFromHighToLow() {
+    this.reviewsArray.sort((a, b) => b.punctuation - a.punctuation);
+  }
+
+  sortFromLowToHigh() {
+    this.reviewsArray.sort((a, b) => a.punctuation - b.punctuation);
+  }
+
+  sortByMostRecent() {
+    this.reviewsArray.sort((a, b) => this.compareDates(b.date, a.date));
+  }
+  
+  sortByOldest() {
+    this.reviewsArray.sort((a, b) => this.compareDates(a.date, b.date));
+  }
+  
+  private compareDates(date1: string, date2: string): number {
+    const parsedDate1 = this.parseDateString(date1);
+    const parsedDate2 = this.parseDateString(date2);
+  
+    return +parsedDate1 - +parsedDate2;
+  }
+  
+  private parseDateString(dateString: string): Date {
+    const [month, day, year] = dateString.split('-').map(Number);
+    return new Date(year, month - 1, day);
   }
 
 }
